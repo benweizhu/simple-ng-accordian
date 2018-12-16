2c24b1aa3914571f0313601e99391ef027b21468
Benwei Zhu <benweizhu@gmail.com>
2018-12-16 13:02:37 +0800

    init expansion-panel and expansion-panel-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpansionPanelComponent } from './expansion-panel/expansion-panel.component';
+import { ExpansionPanelHeaderComponent } from './expansion-panel-header/expansion-panel-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpansionPanelComponent,
+    ExpansionPanelHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/expansion-panel-header/expansion-panel-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expansion-panel-header',
+  templateUrl: './expansion-panel-header.component.html',
+  styleUrls: ['./expansion-panel-header.component.scss']
+})
+export class ExpansionPanelHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
