535c49acaf5d2c5dfeb549b2cdc557572f197b9b
Benwei Zhu <benweizhu@gmail.com>
2018-12-17 11:13:10 +0800

    add to array in service

--- src/app/expansion-panel.service.ts
+++ src/app/expansion-panel.service.ts
@@ -5,7 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class ExpansionPanelService {
 
+  private panels: any[] = [];
+
   constructor() { }
+
+  add(panel: any) {
+    panel && this.panels.push(panel);
+  }
+
+  remove(panel: any) {
+    this.panels = this.panels.filter(x => x !== panel);
+  }
 
   fetchAndExpand() {
     console.log('fetchAndExpand')
--- src/app/expansion-panel/expansion-panel.component.ts
+++ src/app/expansion-panel/expansion-panel.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ExpansionPanelService } from '../expansion-panel.service';
 
 @Component({
   selector: 'app-expansion-panel',
   template: `<ng-content></ng-content>`,
   styleUrls: ['./expansion-panel.component.scss']
 })
-export class ExpansionPanelComponent implements OnInit {
-
-  constructor() { }
+export class ExpansionPanelComponent implements OnInit, OnDestroy {
+  
+  constructor(private expansionPanelService: ExpansionPanelService) { }
 
   ngOnInit() {
+    this.expansionPanelService.add(this);
   }
 
+  ngOnDestroy(): void {
+
+  }
 }
